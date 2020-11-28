6439a7b80fc589236a03a7ff559bdec98df210bb
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2020-11-28 14:06:10 +0100

    Don't throw on failure, possibly set without file

--- api/config.js
+++ api/config.js
@@ -1,10 +1,6 @@
 const dotenv = require('dotenv');
 const result = dotenv.config({ silent: true });
 
-if (result.error) {
-    throw result.error;
-}
-
 const { parsed: envs } = result;
 
 module.exports = envs;
